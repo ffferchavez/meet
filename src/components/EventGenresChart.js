8850758f27c31da842fdac71dd5372202abfc951
Fernando Chavez <ffferchavez@gmail.com>
2024-07-13 12:21:48 +0200

    fixed piechart radius

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -59,7 +59,7 @@ const EventGenresChart = ({ events }) => {
           fill="#8884d8"
           labelLine={false}
           label={renderCustomizedLabel}
-          outerRadius={150}
+          outerRadius={130}
         >
           {data.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
