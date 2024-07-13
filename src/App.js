005e38da072cdcdc6a049528eddbd64067a1d588
Fernando Chavez <ffferchavez@gmail.com>
2024-07-13 10:59:45 +0200

    Added PieChart from recharts

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { extractLocations, getEvents } from "./api";
 import { InfoAlert, ErrorAlert, WarningAlert } from "./components/Alert";
 import CityEventsChart from "./components/CityEventsChart";
+import EventGenresChart from "./components/EventGenresChart";
 
 import "./App.css";
 
@@ -72,7 +73,10 @@ const App = () => {
         updateNumberOfEvents={handleNumberOfEventsChange}
         setErrorAlertText={setErrorAlertText}
       />
-      <CityEventsChart allLocations={allLocations} events={events} />
+      <div className="charts-container">
+        <EventGenresChart events={events} />
+        <CityEventsChart allLocations={allLocations} events={events} />
+      </div>
       <EventList events={events} />
     </div>
   );
